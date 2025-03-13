f9131be72b7a31a49a3cd4d5093c235460228fed
Anton Konyahin <me@konyahin.xyz>
2025-03-13 23:14:31 +0300

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::str;
+
+fn main() -> std::io::Result<()> {
+    let mut stream = TcpStream::connect("sdf.org:70")?;
+    stream.write(b"/\r\n")?;
+
+    loop {
+        let mut answer: [u8; 128] = [0; 128];
+        let size = stream.read(&mut answer)?;
+        let answer = str::from_utf8(&answer).unwrap();
+        println!("{}", answer);
+
+        if size < 128 {
+            break;
+        };
+    }
+    Ok(())
 }
